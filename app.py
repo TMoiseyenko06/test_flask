2035f3c57b2917483d5a5c47f629c5e4f1f35e37
TMoiseyenko06 <tmoiseyenko06@gmail.com>
2024-11-05 15:00:37 -0500

    added get request and updated requirments

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Session
+from sqlalchemy import select
+from flask_marshmallow import Marshmallow
+from marshmallow import fields
 
 app = Flask(__name__)
 app.config.from_object(f'config.DevelopmentConfig')
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class Sum(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,6 +20,17 @@ class Sum(db.Model):
         self.num1 = num1
         self.num2 = num2
         self.result = num1 + num2
+    
+class Schema(ma.Schema):
+    id = fields.Integer()
+    num1 = fields.Integer()
+    num2 = fields.Integer()
+    result = fields.Integer()
+
+    class Meta():
+        fields = ('id','num1','num2','result')
+
+sums_schema = Schema(many=True)
 
 with app.app_context():
     db.create_all()
@@ -28,3 +43,11 @@ def negative_sum(num_1,num_2):
             session.add(new_sum)
             session.commit()
     return jsonify({"sum":int(num_1)+int(num_2)}), 200
+
+@app.route('/filter-by-results/<string:result>',methods=['GET'])
+def filter_results(result):
+    result = int(result)
+    with Session(db.engine) as session:
+        with session.begin():
+            sums = session.query(Sum).all()
+            return sums_schema.jsonify(sums)
